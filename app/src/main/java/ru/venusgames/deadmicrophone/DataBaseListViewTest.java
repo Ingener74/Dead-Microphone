f1eb99206e0ce89c3d95a20244268a01c1bc4c68
ShnaiderPavel <shnaiderpasha@gmail.com>
2016-02-03 10:33:02 +0300

    + авто обновление списка при обновлении базы

--- app/src/main/java/ru/venusgames/deadmicrophone/DataBaseListViewTest.java
+++ app/src/main/java/ru/venusgames/deadmicrophone/DataBaseListViewTest.java
@@ -1,6 +1,10 @@
 package ru.venusgames.deadmicrophone;
 
-import android.database.sqlite.SQLiteDatabase;
+import android.app.LoaderManager;
+import android.content.Context;
+import android.content.CursorLoader;
+import android.content.Loader;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -12,9 +16,11 @@ import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
 
-public class DataBaseListViewTest extends AppCompatActivity {
+public class DataBaseListViewTest extends AppCompatActivity implements android.support.v4.app.LoaderManager.LoaderCallbacks<Cursor> {
 
+    public static final int LOADER_ID = 0;
     private DB database;
+    private SimpleCursorAdapter simpleCursorAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +38,7 @@ public class DataBaseListViewTest extends AppCompatActivity {
                             @Override
                             public void onClick(View v) {
                                 database.clearMessages();
+                                getSupportLoaderManager().getLoader(LOADER_ID).forceLoad();
                             }
                         }).show();
             }
@@ -42,8 +49,10 @@ public class DataBaseListViewTest extends AppCompatActivity {
         String[] fromColumns = new String[]{DB.COLUMN_NAME, DB.COLUMN_MESSAGE};
         int[] toIds = new int[]{R.id.contact_name, R.id.message};
 
+        simpleCursorAdapter = new SimpleCursorAdapter(this, R.layout.contact_item, null, fromColumns, toIds);
+
         ListView listView = (ListView) findViewById(R.id.dataBaseListView);
-        listView.setAdapter(new SimpleCursorAdapter(this, R.layout.contact_item, database.getAllData(), fromColumns, toIds));
+        listView.setAdapter(simpleCursorAdapter);
 
         Button button = (Button) findViewById(R.id.addRec);
         button.setOnClickListener(new View.OnClickListener() {
@@ -52,13 +61,51 @@ public class DataBaseListViewTest extends AppCompatActivity {
                 EditText name = (EditText) findViewById(R.id.editTextName);
                 EditText message = (EditText) findViewById(R.id.editTextMessage);
                 database.addContact(new Contact(name.getText().toString(), message.getText().toString()));
+                getSupportLoaderManager().getLoader(LOADER_ID).forceLoad();
             }
         });
+
+        getSupportLoaderManager().initLoader(LOADER_ID, null, this);
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
         database.close();
+    }
+
+    // LocaderCallbacks<Cursor>
+    @Override
+    public android.support.v4.content.Loader<Cursor> onCreateLoader(int id, Bundle args) {
+        if (id == LOADER_ID) {
+            return new DbCursorLoader(this, database);
+        }
+        return null;
+    }
+
+    @Override
+    public void onLoadFinished(android.support.v4.content.Loader<Cursor> loader, Cursor data) {
+        simpleCursorAdapter.swapCursor(data);
+    }
+
+    @Override
+    public void onLoaderReset(android.support.v4.content.Loader<Cursor> loader) {
+
+    }
+
+    //
+    static class DbCursorLoader extends android.support.v4.content.CursorLoader {
+
+        private DB db;
+
+        public DbCursorLoader(Context context, DB database) {
+            super(context);
+            this.db = database;
+        }
+
+        @Override
+        public Cursor loadInBackground() {
+            return db.getAllData();
+        }
     }
 }
